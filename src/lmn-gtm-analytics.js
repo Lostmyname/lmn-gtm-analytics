6dbd4fee6f1f65fb2a153b31a81428f010105d71
Marc Thomas <hello@mrcthms.com>
2017-06-16 10:57:45 +0100

    Remove track from page call

--- src/lmn-gtm-analytics.js
+++ src/lmn-gtm-analytics.js
@@ -6,7 +6,7 @@ function ensureGTM(timeout) {
   function waitForGTM(resolve, reject) {
     if (typeof dataLayer !== 'undefined') {
       return resolve();
-    } else if (timeout && (Date.now() - start) >= timeout) {
+    } else if (timeout && Date.now() - start >= timeout) {
       return reject(new Error('Timeout'));
     }
     setTimeout(waitForGTM.bind(this, resolve, reject), 50);
@@ -17,119 +17,115 @@ function ensureGTM(timeout) {
  */
 const lmnAnalytics = {
   track: function (action, properties, options, callback) {
-    return ensureGTM()
-      .then(() => {
-        analytics.track.apply(this, arguments);
-        if (typeof options === 'function') {
-          callback = options;
-          options = null;
-        }
-        if (typeof properties === 'function') {
-          callback = properties;
-          options = null;
-          properties = {};
-        }
-        if (!properties) {
-          properties = {};
-        }
-        if (!properties.category) {
-          properties.category = 'All';
-        }
-        dataLayer.push(Object.assign(properties, {
+    return ensureGTM().then(() => {
+      analytics.track.apply(this, arguments);
+      if (typeof options === 'function') {
+        callback = options;
+        options = null;
+      }
+      if (typeof properties === 'function') {
+        callback = properties;
+        options = null;
+        properties = {};
+      }
+      if (!properties) {
+        properties = {};
+      }
+      if (!properties.category) {
+        properties.category = 'All';
+      }
+      dataLayer.push(
+        Object.assign(properties, {
           event: action,
           action: action,
           category: properties.category,
           label: properties.label,
           value: properties.value
-        }));
-        if (callback) {
-          callback();
-        }
-      });
+        })
+      );
+      if (callback) {
+        callback();
+      }
+    });
   },
   page: function (category, name, properties, options, callback) {
-    return ensureGTM()
-      .then(() => {
-        analytics.page.apply(this, arguments);
-        if (typeof options === 'function') {
-          callback = options;
-          options = null;
-        }
-        if (typeof properties === 'function') {
-          callback = properties;
-          options = null;
-          properties = null;
-        }
-        if (typeof name === 'function') {
-          callback =  name;
-          options = null;
-          properties = null;
-          name = null;
-        }
-        if (typeof name === 'object') {
-          options = properties;
-          properties = name;
-          name = null;
-        }
-        if (typeof category === 'string' && typeof name !== 'string') {
-          name = category;
-          category = null;
-        }
-        this.track(`Viewed ${name} Page`, properties, options);
-        if (callback) {
-          callback();
-        }
-        return this;
-      });
-
+    return ensureGTM().then(() => {
+      analytics.page.apply(this, arguments);
+      if (typeof options === 'function') {
+        callback = options;
+        options = null;
+      }
+      if (typeof properties === 'function') {
+        callback = properties;
+        options = null;
+        properties = null;
+      }
+      if (typeof name === 'function') {
+        callback = name;
+        options = null;
+        properties = null;
+        name = null;
+      }
+      if (typeof name === 'object') {
+        options = properties;
+        properties = name;
+        name = null;
+      }
+      if (typeof category === 'string' && typeof name !== 'string') {
+        name = category;
+        category = null;
+      }
+      if (callback) {
+        callback();
+      }
+      return this;
+    });
   },
   identify: function (id, traits, options, callback) {
-    return ensureGTM()
-      .then(() => {
-        analytics.identify.apply(this, arguments);
-        if (typeof options === 'function') {
-          callback = options;
-          options = null;
+    return ensureGTM().then(() => {
+      analytics.identify.apply(this, arguments);
+      if (typeof options === 'function') {
+        callback = options;
+        options = null;
+      }
+      if (typeof traits === 'function') {
+        callback = traits;
+        options = null;
+        traits = null;
+      }
+      dataLayer.push({
+        user: {
+          userId: id
         }
-        if (typeof traits === 'function') {
-          callback = traits;
-          options = null;
-          traits = null;
-        }
-        dataLayer.push({
-          user: {
-            userId: id
+      });
+      if (traits && typeof traits !== 'function') {
+        Object.keys(traits).forEach(trait => {
+          if (trait.startsWith('Experiment:')) {
+            dataLayer.push({
+              experimentName: trait,
+              experimentVariant: traits[trait]
+            });
           }
         });
-        if (traits && typeof traits !== 'function') {
-          Object.keys(traits).forEach((trait) => {
-            if (trait.startsWith('Experiment:')) {
-              dataLayer.push({
-                experimentName: trait,
-                experimentVariant: traits[trait]
-              });
-            }
-          });
-        }
+      }
 
-        if (callback) {
-          callback();
-        }
-        return this;
-      });
+      if (callback) {
+        callback();
+      }
+      return this;
+    });
   },
   impression: function (impressions) {
-    return ensureGTM()
-      .then(() => {
-        impressions.forEach((impression) => {
-          analytics.track('Viewed Impression', impression);
-        });
-        dataLayer.push({
-          ecommerce: {
-            impressions: impressions
-          }
-        });
+    return ensureGTM().then(() => {
+      impressions.forEach(impression => {
+        analytics.track('Viewed Impression', impression);
       });
+      dataLayer.push({
+        ecommerce: {
+          impressions: impressions
+        }
+      });
+    });
   },
   ready: function (callback) {
     if (callback) {
@@ -138,7 +134,5 @@ const lmnAnalytics = {
     return this;
   }
 };
-
-
 
 export default lmnAnalytics;
