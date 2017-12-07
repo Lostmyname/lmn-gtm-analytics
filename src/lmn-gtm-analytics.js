3a897891d50bdea581dc69851237da318badf100
Alex Handley <ahandley@me.com>
2017-12-07 13:46:52 +0000

    Add event metadata to all events
    We need to start sending some metadata with all events to allow for
    sessionization.
    
    Im struggling to test this atm as the library require a DOM, I am
    pushing an alpha now to test on stagin.

--- src/lmn-gtm-analytics.js
+++ src/lmn-gtm-analytics.js
@@ -1,5 +1,8 @@
 /* global analytics, dataLayer */
 
+import Cookies from 'js-cookie';
+import uuid from 'uuid';
+
 function ensureSetup(timeout) {
   var start = Date.now();
   return new Promise(waitForGTM);
@@ -12,13 +15,43 @@ function ensureSetup(timeout) {
     setTimeout(waitForGTM.bind(this, resolve, reject), 50);
   }
 }
+
+function getClientUuid() {
+  var foundCookie = Cookies.get('clientUuid');
+  if (foundCookie) {
+    return foundCookie;
+  }
+
+  var newUuid = uuid.v4();
+  Cookies.set('clientUuid', newUuid, { expires: 365 });
+  return newUuid;
+}
+
+function getGaCookieId() {
+  return Cookies.get('_ga') || null;
+}
+
+function eventMetaData() {
+  return {
+    sentTimestamp: + new Date(),
+    clientUuid: getClientUuid(),
+    gaCookieId: getGaCookieId()
+  };
+}
+
+function argumentsWithEventMetaData(args) {
+  args[1] = Object.assign(args[1] || {}, eventMetaData());
+
+  return args;
+}
+
 /**
  * Analytics wrapper for the Segment to GTM integration
  */
 const lmnAnalytics = {
   track: function (action, properties, options, callback) {
     return ensureSetup().then(() => {
-      analytics.track.apply(this, arguments);
+      analytics.track.apply(this, argumentsWithEventMetaData(arguments));
       if (typeof options === 'function') {
         callback = options;
         options = null;
@@ -34,6 +67,9 @@ const lmnAnalytics = {
       if (!properties.category) {
         properties.category = 'All';
       }
+
+      Object.assign(properties, eventMetaData());
+
       dataLayer.push(
         Object.assign(properties, {
           event: action,
@@ -50,7 +86,7 @@ const lmnAnalytics = {
   },
   page: function (category, name, properties, options, callback) {
     return ensureSetup().then(() => {
-      analytics.page.apply(this, arguments);
+      analytics.page.apply(this, argumentsWithEventMetaData(arguments));
       if (typeof options === 'function') {
         callback = options;
         options = null;
@@ -78,13 +114,13 @@ const lmnAnalytics = {
       if (callback) {
         callback();
       }
-      dataLayer.push(properties);
+      dataLayer.push(Object.assign(properties, eventMetaData()));
       return this;
     });
   },
   identify: function (id, traits, options, callback) {
     return ensureSetup().then(() => {
-      analytics.identify.apply(this, arguments);
+      analytics.identify.apply(this, argumentsWithEventMetaData(arguments));
       if (typeof options === 'function') {
         callback = options;
         options = null;
@@ -94,18 +130,18 @@ const lmnAnalytics = {
         options = null;
         traits = null;
       }
-      dataLayer.push({
-        user: {
-          userId: id
-        }
-      });
+      dataLayer.push(
+        Object.assign(eventMetaData(),
+        { user: { userId: id } }
+      ));
+
       if (traits && typeof traits !== 'function') {
         Object.keys(traits).forEach(trait => {
           if (trait.startsWith('Experiment:')) {
-            dataLayer.push({
-              experimentName: trait,
-              experimentVariant: traits[trait]
-            });
+            dataLayer.push(
+              Object.assign(eventMetaData(),
+              { experimentName: trait, experimentVariant: traits[trait] }
+            ));
           }
         });
       }
@@ -121,11 +157,10 @@ const lmnAnalytics = {
       impressions.forEach(impression => {
         analytics.track('Viewed Impression', impression);
       });
-      dataLayer.push({
-        ecommerce: {
-          impressions: impressions
-        }
-      });
+      dataLayer.push(
+        Object.assign(eventMetaData(),
+        { ecommerce: { impressions: impressions } }
+      ));
     });
   },
   ready: function (callback) {
